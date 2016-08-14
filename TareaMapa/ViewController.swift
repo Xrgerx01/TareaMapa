482599e124d3227fb8c524377af609dd7d58e9f4
Xrgerx01 <xrgerx@xrgerxs-imac.local>
2016-08-14 13:11:35 -0700

    nuevo

--- TareaMapa/ViewController.swift
+++ TareaMapa/ViewController.swift
@@ -7,19 +7,70 @@
 //
 
 import UIKit
+import MapKit
+import CoreLocation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, CLLocationManagerDelegate {
 
+    @IBOutlet weak var mapa: MKMapView!
+    private let manejador = CLLocationManager()
+    private var distanciaRecorrida: Double = 0.0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        manejador.delegate = self
+        manejador.desiredAccuracy = kCLLocationAccuracyBest
+        manejador.requestWhenInUseAuthorization()
+        manejador.distanceFilter = 50.0
+        mapa.zoomEnabled = true
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func locationManager(manager: CLLocationManager, didChangeAuthorizationStatus status: CLAuthorizationStatus) {
+        if status == .AuthorizedWhenInUse {
+            manejador.startUpdatingLocation()
+            mapa.showsUserLocation = true
+            //mapa.centerCoordinate
+        } else {
+            manejador.stopUpdatingLocation()
+            mapa.showsUserLocation = false
+        }
     }
-
+    
+    func locationManager(manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        var puntoactual = CLLocationCoordinate2D()
+         puntoactual.latitude = manager.location!.coordinate.latitude
+         puntoactual.longitude = manager.location!.coordinate.longitude
+         distanciaRecorrida = distanciaRecorrida + 50.0
+         let pinactual = MKPointAnnotation()
+         pinactual.title = "Lat.: \(Float(puntoactual.latitude)) , Long: \(Float(puntoactual.longitude))"
+         pinactual.subtitle = "\(distanciaRecorrida)"
+         pinactual.coordinate = puntoactual
+         mapa.addAnnotation(pinactual)
+    }
+    
+    func locationManager(manager: CLLocationManager, didFailWithError error: NSError) {
+        let alerta = UIAlertController(title: "Error", message: "error \(error.code)", preferredStyle: .Alert)
+        let accionOk = UIAlertAction(title: "OK", style: .Default, handler: {
+            accion in
+            //..
+        })
+        
+        alerta.addAction(accionOk)
+        self.presentViewController(alerta, animated: true, completion: nil)
+    }
+    
+    @IBAction func mapaNormal() {
+        mapa.mapType = MKMapType.Standard
+    }
+    
+    @IBAction func mapaSatelital() {
+        mapa.mapType = MKMapType.Satellite
+    }
+    
+    @IBAction func mapaHibrido() {
+        mapa.mapType = MKMapType.Hybrid
+    }
 
 }
 
